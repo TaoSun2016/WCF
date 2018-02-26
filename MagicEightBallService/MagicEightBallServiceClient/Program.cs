96a7bff6168fcf3bd4f59b882e0e76e5c67e8c04
TaoSun2016 <taosun.developer@gmail.com>
2018-02-26 19:23:18 +1300

    add service reference

--- MagicEightBallService/MagicEightBallServiceClient/Program.cs
+++ MagicEightBallService/MagicEightBallServiceClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MagicEightBallServiceClient.ServiceReference1;
 
 namespace MagicEightBallServiceClient
 {
@@ -10,6 +11,15 @@ namespace MagicEightBallServiceClient
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Ask the Magic 8 Ball * ****\n");
+            using (EightBallClient ball = new EightBallClient())
+            {
+                Console.Write("Your question: ");
+                string question = Console.ReadLine();
+                string answer =  ball.ObtainAnswerToQuestion(question);
+                Console.WriteLine("8-Ball says: {0}", answer);
+            }
+            Console.ReadLine();
         }
     }
 }
