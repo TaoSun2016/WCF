7ec866c19e68caf2fe310f36696ad0809be83453
TaoSun2016 <taosun.developer@gmail.com>
2018-03-03 16:43:54 +1300

    finish client codes

--- MathServiceLibrary/MathClient/Program.cs
+++ MathServiceLibrary/MathClient/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathClient.ServiceReference1;
+using System.Threading;
 
 namespace MathClient
 {
@@ -10,6 +12,22 @@ namespace MathClient
     {
         static void Main(string[] args)
         {
+            using (BasicMathClient proxy = new BasicMathClient())
+            {
+                proxy.Open();
+                IAsyncResult result = proxy.BeginAdd(2, 3, ar =>
+                    {
+                        Console.WriteLine("2 + 3 = {0}",
+                        proxy.EndAdd(ar));
+                    },
+                    null);
+                while (!result.IsCompleted)
+                {
+                    Thread.Sleep(200);
+                    Console.WriteLine("Client working...");
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
